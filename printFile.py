6ec48f9038ece61977b96d8518afae2c8cd9d203
femianthony11 <60587771+femianthony11@users.noreply.github.com>
2021-06-21 18:36:36 -0400

    Add files via upload
    Unfinished printFile, only has 2 of the questions answered

--- printFile.py
+++ printFile.py
@@ -4,14 +4,33 @@ print(sys.argv)
 args = sys.argv
 
 filepath = args[1]
+
+
+print(sys.argv[2])	
+
+
 p=int(args[2])
 
-a_file = open(filepath)
+num_lines = sum(1 for line in open(args[1]))
 
+a_file = open(filepath)	
 number_of_lines = p
 
-for p in range(number_of_lines):
-	line = a_file.readline()
-	print(line)
+if p < 0:
+	p=-p
 
-print("Program complete")
+if os.path.exists(args[1]) == False:
+	print("File does not exist...")
+	quit()
+
+
+if int(args[2]) > num_lines:
+	print('Not enough lines in the file, exiting.... ')
+	quit()
+
+
+else:
+
+	for p in range(number_of_lines):
+		line = a_file.readline()
+		print(line)
