6d4bb1d016b77c1dc6b7ebf72dbfdb9b1eab3891
femianthony11 <psfcacademy@gmail.com>
2021-06-24 21:26:20 -0400

    printFile.py

--- printFile.py
+++ printFile.py
@@ -2,9 +2,6 @@ import os.path
 import sys
 from traceback import format_tb
 
-print(sys.argv)
-args = sys.argv
-
 def isInt(s):
     try: 
         int(s)
@@ -12,51 +9,64 @@ def isInt(s):
     except ValueError:
         return False
 
-if isInt(args[2]) == False:
+
+
+print(sys.argv)
+
+args = sys.argv
+
+filename = (args[1])
+
+number_of_lines = (args[2])
+
+
+
+
+if isInt(number_of_lines) == False:
 	print('n must be an integer')
 	quit()
 
-number_of_lines = (args[2])
-z = number_of_lines
-z = int(number_of_lines)
-
+number_of_lines = int(number_of_lines)
 
 #If the file doesn't exist, it should exit and print:
-if os.path.exists(args[1]) == False:
+if os.path.exists(filename) == False:
 	print("File does not exist...")
 	quit()
-print(int(args[2]))
 
 
 
-
-f = open("notes.txt", "r")
-
-num_lines = sum(1 for line in open(args[1]))
+f = open(filename, "r")
 
 
 
-xs = f.readlines()
+lines = f.readlines()
+
+filelength=len(lines)
 
 
+
+if int(number_of_lines) > filelength:
+	print('Not enough lines in the file, exiting.... ')
+	quit()
+
+print(f"filelength = {filelength}")
+print(f"number_of_lines = {number_of_lines}")
 
 
 #If the argument n is negative e.g. -10, it should print out the last 10 lines .
-if z < 0:
-	print(xs[z:])
-	quit()
+if number_of_lines < 0:
+	start = filelength+number_of_lines
+	end=filelength
+	print(f"start = {start}")
+	print(f"end = {end}")
+else:
+	start = 0
+	end=number_of_lines
 
 
 
 
 #If the number of lines to print n, is more than the total number of lines in the file, it should exit and print
-if int(args[2]) > num_lines:
-	print('Not enough lines in the file, exiting.... ')
-	quit()
 
-
-else:
-
-	for i in range(z):
-		line = a_file.readline()
-		print(line)
+for i in range(start,end):
+	print(lines[i])
