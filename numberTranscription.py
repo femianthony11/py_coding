5e49868ce2b21663f137e94c02bdd116d9291796
femianthony11 <psfcacademy@gmail.com>
2021-07-14 19:48:26 -0400

    Changes 2.2

--- numberTranscription.py
+++ numberTranscription.py
@@ -3,7 +3,7 @@ import sys
 from traceback import format_tb
 import re
 args = sys.argv
-nums20 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
+nums20 = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
             6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
             11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
             15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen', 100: 'One-Hundred'}
@@ -27,13 +27,13 @@ def conv_to_words(n):
         print('Number is out of range')
         quit()
     if n == 0:
-        print("Only print from numbers 1 to 100 please")
+        print('Your number converted is' + ' ' + (nums20[n]))
         quit()
     if (n >= 1) and (n < 20) or (n == 100) or (n == 10):
         print('Your number converted is' + ' ' + (nums20[n]))
     elif nums[1] == 0 and int(n) != 10:
         print('Your number converted is:' + numstens[n])
-    elif (n > 20) and (n < 99):
+    elif (n > 20) and (n <= 99):
         n1 = nums[0]
         n2 = nums[1]
         print('Your number converted is:' + numtens[n1] +" " + nums20[n2].lower())
