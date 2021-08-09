8d6e0ee781f598ac09e023bbd40bfc7e9e1cc2ec
femianthony11 <psfcacademy@gmail.com>
2021-08-09 17:16:25 -0400

    complexnums.py

--- complexnums.py
+++ complexnums.py
@@ -17,7 +17,7 @@ class ComplexNumber:
 
     def __init__(self,a,b):
         self.a = a
-        self.b = b 
+        self.b = b
 
     def magn(self):
         xy = self.a**2 + self.b**2
@@ -26,36 +26,61 @@ class ComplexNumber:
 
 
 
-    def conj1(self):
-        conjnum = ComplexNumber(self.a, -self.b)
+    def conj1(self,a,b):
+        conjnum = ComplexNumber(self.a,-self.b,)
         return conjnum
 # Check if self.b is negative!
+
+    def __add__(self,other,other1):
+        addi =  ComplexNumber(self.a+other, self.b+other1)
+        return addi
+    def __sub__(self, other,other1):
+        sub = complex(self.a-other, self.b-other1)
+        return sub
+    def __mul__(self, other,other1):
+        mul = complex(self.a*other-self.b*other1, self.a*other1+self.b*other)
+        return mul
     def __str__(self):
         if int(self.b) < 0:
             return (str(self.a)+ str(self.b)+"i")
         else:
             return (str(self.a) +"+" + str(self.b)+"i")
+def op(ope):
+    return (f"Operation = {ope}")
+
+
 
 def main():
     arg1 = (input("Enter number 1: "))
     arg2 = (input("Enter number 2: "))
+    arg3 = (input("Enter number 3: "))
+    arg4 = (input("Enter number 4: "))
+    arg5 = input("Enter Operation( + , - , x) : ")
 
-    #if isInt(arg1) == False:
-    #    print("PLEASE INPUT NUMBER")
-    #    quit()
-    #elif isInt(arg1) == False:
-    #
-    #    quit()
+
 
     arg1 = int(arg1)
     arg2 = int(arg2)
-    compnum = ComplexNumber(arg1,arg2)
+    arg3 = int(arg3)
+    arg4 = int(arg4)
+    compnum = ComplexNumber(arg1,arg2,)
 
     magnitude = compnum.magn()
-    conjugate = compnum.conj1()
-
+    conjugate = compnum.conj1(arg1,arg2)
+    add = compnum.__add__(arg3,arg4)
+    sub = compnum.__sub__(arg3,arg4)
+    mul = compnum.__mul__(arg3,arg4)
     print(f"Magnitude = {magnitude}")
     print(f" Conjugate = {conjugate}")
+
+    if (arg5 == '+'):
+        print(op(add))
+    elif (arg5 == '-'):
+        print(op(sub))
+    elif (arg5 == 'x'):
+        print(op(mul))
+    
+
 
 if __name__ == "__main__":
     main()
--- testComplexNumbers.py
+++ testComplexNumbers.py
@@ -21,6 +21,8 @@ class compTest(unittest.TestCase):
                 "9,4":"9.848857801796104", 
                 "6,2":"6.324555320336759"}
 
+        self.oplist = {"z1+z2":"9+i","z1-z2":"1+5i","z1*z2":"26+2i"}
+
 
     def test_fact(self):
 
@@ -37,10 +39,44 @@ class compTest(unittest.TestCase):
             arg1 = int(item[0])
             arg2 = int(item[2])
             cn = ComplexNumber(arg1,arg2)
-            conjugate = cn.conj1()
+            conjugate = cn.conj1(arg1,arg2)
             print(conjugate)
             res = str(conjugate)
             self.assertEqual(res,self.conjlist[item])
+        for item in self.oplist:
+            z1 = ("5+3i")
+            z2 = ("4-2i")
+            a1 = item[2]
+            n1 = int(z1[0])
+            n2 = int(z1[2])
+            n3 = int(z2[0])
+            n4 = -(int(z2[2]))
+            ans = ComplexNumber(n1,n2)
+            add = ans.__add__(n3,n4)
+
+            sub = ans.__sub__(n3,n4)
+            mul = ans.__mul__(n3,n4)
+            
+
+            if (a1 == '+'):
+                res = str(add)
+                res = res.replace("1","")
+                res = res.replace("(","")
+                res = res.replace(")","")
+                res = res.replace("j","i")
+            elif (a1 == '-'):
+                res = str(sub)
+                res = res.replace("(","")
+                res = res.replace(")","")
+                res = res.replace("j","i")
+            elif (a1 == '*'):
+                res =str(mul)
+                res = res.replace("(","")
+                res = res.replace(")","")
+                res = res.replace("j","i")
+    
+            self.assertEqual(res,self.oplist[item])
+
 
 
 if __name__ == "__main__":
