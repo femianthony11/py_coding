443680f49ab675f2f1b68471e01316ca2469a50f
femianthony11 <60587771+femianthony11@users.noreply.github.com>
2021-12-19 09:04:27 -0500

    Add files via upload

---
+++ starturtle.py
@@ -0,0 +1,19 @@
+# import turtle
+import turtle
+ 
+# Starting a screen
+ws = turtle.Screen()
+ 
+# initializing the turtle
+turtle = turtle.Turtle()
+# editing width
+turtle.width(15)
+
+# executing loop 5 times for a stars points
+for i in range(5):
+ 
+        # moving turtle 200 units forward
+        turtle.forward(300)
+ 
+        # rotating turtle 144 degree right
+        turtle.right(144)
